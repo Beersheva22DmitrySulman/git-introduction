58109c47c7b99e3202e9919556a1c10322765a92
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-11-11 17:28:54 +0200

    HW 6

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -12,13 +12,13 @@ public class MyArrays {
 		res[res.length - 1] = num;
 		return res;
 	}
-	
+
 	/**
 	 * 
 	 * @param array
 	 * @param index
-	 * @return new array with no number at a given index in a given array
-	 * if index doesn't exist in a given array it returns the same array
+	 * @return new array with no number at a given index in a given array if index
+	 *         doesn't exist in a given array it returns the same array
 	 */
 	public static int[] removeNumber(int[] array, int index) {
 		int[] res = array;
@@ -29,7 +29,7 @@ public class MyArrays {
 		}
 		return res;
 	}
-	
+
 	/**
 	 * 
 	 * @param arraySorted
@@ -37,14 +37,14 @@ public class MyArrays {
 	 * @return new array with inserted number at an index for keeping array sorted
 	 */
 	public static int[] insertSorted(int[] arraySorted, int number) {
-		int index = Arrays.binarySearch(arraySorted , number);
+		int index = Arrays.binarySearch(arraySorted, number);
 		if (index < 0) {
 			index = -1 - index;
 		}
-		
+
 		return insertAtIndex(arraySorted, number, index);
 	}
-	
+
 	private static int[] insertAtIndex(int[] array, int number, int index) {
 		int[] res = new int[array.length + 1];
 		res[index] = number;
@@ -52,7 +52,7 @@ public class MyArrays {
 		System.arraycopy(array, index, res, index + 1, array.length - index);
 		return res;
 	}
-	
+
 	public static int binarySearch(int[] array, int number) {
 		int l = 0;
 		int r = array.length - 1;
@@ -70,4 +70,82 @@ public class MyArrays {
 		}
 		return res;
 	}
+	
+	public static int count;
+
+	public static void bubbleSort(int[] array) {
+		count = 0;
+		int nextInterationLimit = array.length - 1;
+		for (int i = 0; i < array.length - 1; i++) {
+			nextInterationLimit = moveGreaterRight(array, nextInterationLimit);
+			if (nextInterationLimit == -1) {
+				break;
+			}
+		}
+		System.out.println(count);
+	}
+	
+	private static int moveGreaterRight(int[] array, int length) {
+		int lastSwapIndex = -1;
+		for (int j = 0; j < length; j++) {
+			count++;
+			if (array[j + 1] < array[j]) {
+				lastSwapIndex = j;
+				swapElements(array, j, j + 1);
+			}
+		}
+		return lastSwapIndex;
+	}
+	
+	private static void swapElements(int[] array, int index1, int index2) {
+		int swap = array[index1];
+		array[index1] = array[index2];
+		array[index2] = swap;
+	}
+
+	public static boolean checkArray(int[] array) {
+		int[] arrayCopy = Arrays.copyOf(array, array.length);
+		boolean res = false;
+		int bugsCount = 0;
+		int bugIndex = -1;
+		for (int i = 1; i < arrayCopy.length; i++) {
+			if (arrayCopy[i] < arrayCopy[i - 1]) {
+				if (res) {
+					res = false;
+					break;
+				}
+				bugsCount++;
+				if (bugIndex == -1) {
+					bugIndex = i - 1;
+				}
+			}
+			if (!res && bugIndex != -1) {
+				swapElements(arrayCopy, bugIndex, i);
+				res = isNubmerAtIndexValid(arrayCopy, bugIndex) && isNubmerAtIndexValid(arrayCopy, i);
+				if (res) {
+					bugsCount--;
+				} else {
+					swapElements(arrayCopy, bugIndex, i);
+				}
+			}
+			if (bugsCount == 2) {
+				res = false;
+				break;
+			}
+		}
+
+		return res;
+	}
+
+	private static boolean isNubmerAtIndexValid(int[] array, int index) {
+		boolean res = true;
+		if (index > 0 && array[index - 1] > array[index]) {
+			res = false;
+		}
+		if (res && index < array.length - 1 && array[index + 1] < array[index]) {
+			res = false;
+		}
+		return res;
+	}
+
 }
--- src/MyArraysTest.java
+++ src/MyArraysTest.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class MyArraysTest {
 	@Test
@@ -51,6 +52,8 @@ public class MyArraysTest {
 	void binarySearchTest() {
 		assertEquals(2, MyArrays.binarySearch(new int[] {1, 2, 3, 4, 6, 7, 8, 9, 10}, 3));
 		assertEquals(3, MyArrays.binarySearch(new int[] {0, 1, 2, 3, 3, 3, 8, 9, 10}, 3));
+		assertEquals(2, MyArrays.binarySearch(new int[] {0, 1, 2, 3, 3, 3, 8, 9, 10}, 2));
+		assertEquals(8, MyArrays.binarySearch(new int[] {0, 1, 2, 3, 3, 3, 8, 9, 10}, 10));
 		assertEquals(0, MyArrays.binarySearch(new int[] {3, 3, 3, 3, 3, 3, 8, 9, 10}, 3));
 		assertEquals(0, MyArrays.binarySearch(new int[] {3, 3, 3, 3, 3, 3, 3, 3, 3}, 3));
 		assertEquals(0, MyArrays.binarySearch(new int[] {3, 4, 5, 6}, 3));
@@ -61,5 +64,56 @@ public class MyArraysTest {
 		assertEquals(-5, MyArrays.binarySearch(new int[] {1, 2, 3, 4, 6, 6, 6, 9, 10}, 5));
 		assertEquals(-5, MyArrays.binarySearch(new int[] {1, 4, 4, 4, 6, 6, 6, 9, 10}, 5));
 		assertEquals(-2, MyArrays.binarySearch(new int[] {1, 4, 4, 4, 6, 6, 6, 9, 10}, 2));
+		assertEquals(-1, MyArrays.binarySearch(new int[] {1, 4, 4, 4, 6, 6, 6, 9, 10}, 0));
+		assertEquals(-9, MyArrays.binarySearch(new int[] {1, 4, 4, 4, 6, 6, 6, 9, 10}, 11));
+	}
+	
+	@Test
+	@Timeout(1)
+	void bubbleSort() {
+		int[] array1 = {7, 36, 9, 8, 1, 0, 5, 12, 77, 6, 1};
+		MyArrays.bubbleSort(array1);
+		assertArrayEquals(new int[] {0, 1, 1, 5, 6, 7, 8, 9, 12, 36, 77}, array1);
+		
+		int[] array2 = {2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2};
+		MyArrays.bubbleSort(array2);
+		assertArrayEquals(new int[] {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2}, array2);
+		
+		int[] array3 = {200, 150, 100, 50, 40, 30, 20, 12, 10, 10, 2};
+		MyArrays.bubbleSort(array3);
+		assertArrayEquals(new int[] {2, 10, 10, 12, 20, 30, 40, 50, 100, 150, 200}, array3);
+		
+		int[] arrayMillionTwos = new int[1_000_000];
+		for (int i = 0; i < 1_000_000; i++) {
+			arrayMillionTwos[i] = 2;
+		}
+		MyArrays.bubbleSort(arrayMillionTwos);
+	}
+	
+	@Test
+//	@Disabled
+	void checkArrayTest() {
+		assertTrue(MyArrays.checkArray(new int[] {10, 1, 2, 3, 4, 5, 6, 7, 0}));
+		assertTrue(MyArrays.checkArray(new int[] {1, 0, 2, 3, 4, 5, 6, 7, 8}));
+		assertTrue(MyArrays.checkArray(new int[] {0, 0, 1, 3, 4, 5, 6, 9, 7}));
+		assertTrue(MyArrays.checkArray(new int[] {0, 7, 1, 3, 4, 5, 6, 0, 9}));
+		assertTrue(MyArrays.checkArray(new int[] {0, 7, 2, 3, 4, 5, 6, 1, 9}));
+		assertTrue(MyArrays.checkArray(new int[] {1, 2, 3, 10, 16, 11, 20}));
+		assertTrue(MyArrays.checkArray(new int[] {1, 2, 3, 10, 16, 11, 11, 10, 20}));
+		assertTrue(MyArrays.checkArray(new int[] {1, 2, 3, 10, 16, 11, 11, 11, 20}));
+		assertTrue(MyArrays.checkArray(new int[] {5, 4, 4, 4, 4}));
+		assertTrue(MyArrays.checkArray(new int[] {5, 5, 5, 5, 5, 6, 5}));
+		assertTrue(MyArrays.checkArray(new int[] {10, 1}));
+		
+		assertFalse(MyArrays.checkArray(new int[] {1, 2, 3, 4, 5, 6, 6, 7}));
+		assertFalse(MyArrays.checkArray(new int[] {5, 4, 4, 2, 1}));
+		assertFalse(MyArrays.checkArray(new int[] {0, 10, 2, 11, 11, 12, 13, 1, 90}));
+		assertFalse(MyArrays.checkArray(new int[] {2, 10, 4, 11, 11, 12, 13, 1, 90}));
+		assertFalse(MyArrays.checkArray(new int[] {10, 0, 1, 3, 4, 5, 6, 7, 9}));
+		assertFalse(MyArrays.checkArray(new int[] {1, 3, 4, 6, 12, 10, 11, 12, 13, 14}));
+		assertFalse(MyArrays.checkArray(new int[] {1, 2, 3, 10, 16, 14, 11, 11, 11, 20}));
+		assertFalse(MyArrays.checkArray(new int[] {5, 5, 5, 5, 5, 5, 5}));
+		assertFalse(MyArrays.checkArray(new int[] {5, 5, 5, 5, 5, 5, 6}));
+		assertFalse(MyArrays.checkArray(new int[] {1, 10}));
 	}
 }
